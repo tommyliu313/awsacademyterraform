464891ed060e7267ab438f38f5518f021afa9110
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-23 17:07:34 +0800

    20220423 1707 updaye

--- architecture/Migrating_a_database_to_amazon_rds/module.tf
+++ architecture/Migrating_a_database_to_amazon_rds/module.tf
@@ -1,5 +1,18 @@
 module "rds" {
   source  = "terraform-aws-modules/rds/aws"
   version = "4.2.0"
-  # insert the 38 required variables here
+  # database engine
+  engine = "mysql"
+  engine_version = ""
+  instance_class = ""
+  allocated_storage = 3
+
+  # database info
+  db_name = ""
+  username = ""
+  port = "3306"
+  identifier = "database-1"
+
+  create_db_instance = true
+  create_db_subnet_group = true
 }
--- architecture/Migrating_a_database_to_amazon_rds/resource.tf
+++ architecture/Migrating_a_database_to_amazon_rds/resource.tf
@@ -1,1 +1,8 @@
-resource "database"
+resource "aws_db_instance" ""{
+allocated_storage = 20
+engine = "mysql"
+instance_class=""
+name = ""
+username = "admin"
+password = "Caf3DbPassw0rd!"
+}
