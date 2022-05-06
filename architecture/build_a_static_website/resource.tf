3c8294731d9b71ed46e5b45b6acfeed86b02062f
Tommyliu313 <[liuchaktin@gmail.com]>
2022-05-06 21:15:47 +0800

    20220506 2115

--- architecture/build_a_static_website/resource.tf
+++ architecture/build_a_static_website/resource.tf
@@ -1,10 +1,12 @@
 resource "aws_s3_bucket" "website-123"{
  bucket = "website123"
+ region = var.region
   tags = {
    Department = "Marketing"
   }
   website {
     error_document = "error.html"
+    index_document = "index.html"
   }
 }
 
@@ -15,4 +17,16 @@ resource "aws_s3_bucket_website_configuration" ""{
   }
 }
 
-resource "aws_s3_bucket_object" ""{}
+resource "aws_s3_bucket_object" "script"{
+  bucket = aws_s3_bucket.website-123
+  key = ""
+  source = ""
+}
+
+
+resource "aws_s3_bucket_public_access_block" "publicaccess"{
+
+  bucket = aws_s3_bucket.website-123
+  block_public_acls = false
+  block_public_policy = false
+}
